a46bcc916be7854f3222a736378bb546ba0a829b
tekostyshyn <tekostyshyn@gmail.com>
2023-04-27 20:15:39 +0300

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,58 +1,51 @@
-import { useReducer } from 'react';
+import { useState } from 'react';
 import Section from './Section';
 import Statistics from './Statistics';
 import FeedbackOptions from './FeedbackOptions';
 import Notification from './Notification';
 
-const initialState = { good: 0, neutral: 0, bad: 0, total: 0 };
-const options = ["good", "neutral", "bad"];
-
-function reducer(state, action) {
-  switch (action.type) {
-    case 'good':
-      return { ...state, good: state.good + 1 };
-    case 'neutral':
-      return { ...state, neutral: state.neutral + 1 };
-    case 'bad':
-      return { ...state, bad: state.bad + 1 };
-    case 'countTotal':
-      return { ...state, total: state.good + state.neutral + state.bad };
-    default:
-      throw new Error();
-  }
-}
+const options = ['good', 'neutral', 'bad'];
 
 export const App = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   const countTotalFeedback = () => {
-    const { good, neutral, bad } = state;
     return good + neutral + bad;
   };
 
   const countPositiveFeedbackPercentage = () => {
-    return Math.round((state.good / countTotalFeedback()) * 100);
+    return Math.round((good / countTotalFeedback()) * 100);
   };
 
   const leaveFeedback = option => {
-    dispatch({ type: `${option}` });
-    dispatch({ type: 'countTotal' });
+    switch (option) {
+      case 'good':
+        setGood(good + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+      case 'bad':
+        setBad(bad + 1);
+        break;
+      default:
+        console.log('There is no such state');
+    }
   };
 
   return (
     <div>
       <Section title="Please leave feedback">
-        <FeedbackOptions
-          options={options}
-          onLeaveFeedback={leaveFeedback}
-        ></FeedbackOptions>
+        <FeedbackOptions options={options} onLeaveFeedback={leaveFeedback} />
       </Section>
       <Section title="Statistics">
         {countTotalFeedback() > 0 ? (
           <Statistics
-            good={state.good}
-            bad={state.bad}
-            neutral={state.neutral}
+            good={good}
+            bad={bad}
+            neutral={neutral}
             total={countTotalFeedback()}
             positivePercentage={countPositiveFeedbackPercentage()}
           />
